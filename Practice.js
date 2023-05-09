4ccdc20453e68dcf11d0033643eff9e92c2c2666
Muslim-Abubakr <ismusis@yandex.ru>
2023-05-09 15:47:27 +0300

    complete task

--- Practice.js
+++ Practice.js
@@ -26,3 +26,18 @@ function exponent(a,b){
 function subt(a,b){
     return a - b
 }
+
+// Collatz Conjecture (3n+1)
+
+var hotpo = function(n){
+    var c = 0;
+    
+      while (n > 1) {
+      n = n % 2 ? 3 * n + 1 : n / 2;
+      c++;
+    }
+  
+      return c;
+  }
+
+  
