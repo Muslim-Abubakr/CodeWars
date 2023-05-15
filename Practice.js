408bb8b1741480ef3d11aca59d2f81aed6593dd8
Muslim-Abubakr <ismusis@yandex.ru>
2023-05-15 17:55:23 +0300

    added

--- Practice.js
+++ Practice.js
@@ -49,3 +49,16 @@ var hotpo = function(n){
   function reverse(string){
     return string.split(' ').reverse().join(' ')
   }
+
+  
+  function bmi(weight, height) {
+    if(weight / height ** 2 <= 18.5) {
+      return 'Underweight'
+    } else if (weight / height ** 2 <= 25.0) {
+      return 'Normal'
+    } else if (weight / height ** 2 <= 30.0) {
+      return 'Overweight'
+    } else if (weight / height ** 2 > 30) {
+      return 'Obese'
+    }
+  }
