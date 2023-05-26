d8671f4efbd4de81757e4e627f9d9accc42cd0b1
Muslim-Abubakr <ismusis@yandex.ru>
2023-05-26 20:01:59 +0300

    added practice

--- Practice.js
+++ Practice.js
@@ -80,3 +80,13 @@ function makeUpperCase(str) {
  }
 
  correct = s => s.replace(/0/g,'O').replace(/1/g,'I').replace(/5/g,'S')
+
+ const corrections = {
+	'5': 'S',
+	'0': 'O',
+  '1': 'I',
+};
+
+const correct = string => (
+	string.replace(/[501]/g, character => corrections[character])
+);
