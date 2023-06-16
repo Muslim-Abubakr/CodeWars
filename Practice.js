de283907a9c88bb6949b03a7c78f2b4486e6659e
Muslim-Abubakr <ismusis@yandex.ru>
2023-06-16 14:16:23 +0300

    completed tasks from codewars

--- Practice.js
+++ Practice.js
@@ -387,3 +387,13 @@ function repeatStr (n, s) {
   }
   return count
 } 
+
+// Square(n) Sum
+
+function squareSum(numbers){
+  sum = 0
+  for(i = 0; i < numbers.length; i++) {
+    sum += numbers[i] ** 2
+  }
+  return sum
+}
