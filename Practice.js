ffff1a9412a9a55ebb86c20d3cbd24ede082bbe6
Muslim-Abubakr <ismusis@yandex.ru>
2023-07-06 02:59:16 +0300

    added task

--- Practice.js
+++ Practice.js
@@ -435,4 +435,16 @@ Const myCity = {
 
 myCity.cityGreeting() // выведет 'Hi'
 
+// Counting sheep...
+
+function countSheeps(arrayOfSheep) {
+  let counter = 0
+  for (let i = 0; i < arrayOfSheep.length; i++) {
+    if (arrayOfSheep[i] === true) {
+      counter++
+    }
+  }
+  return counter
+    // TODO May the force be with you
+}
 
